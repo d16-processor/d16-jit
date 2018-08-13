20b5f699667985c0fc36ade942b6313adf5d2dd4
C-Elegans <mtnolan2640@gmail.com>
2018-08-13 13:38:58 -0400

    Improve the artificial delay while reading from the uart status register

--- io.c
+++ io.c
@@ -19,6 +19,7 @@ int fd[2];
 int read_fd[2];
 bool thread_run = true;
 volatile atomic_uint bytes_tx, bytes_rx;
+volatile atomic_uint write_previous = 0;
 void* io_thread_write_process(void * unused){
 
 	char buf[128];
@@ -26,12 +27,12 @@ void* io_thread_write_process(void * unused){
 	while(thread_run){
 		while((bytes = read(fd[0], buf, sizeof(buf))) != EOF){
 			fwrite(buf, 1, bytes, stdout);
-            unsigned int btx = 0;
-           if(bytes > 0)
-                btx = atomic_fetch_add(&bytes_tx,bytes); 
-            /*printf("bytes_tx (read) %d\n",btx);*/
+			fflush(stdout);
+			unsigned int btx = 0;
+			if(bytes > 0)
+			  btx = atomic_fetch_add(&bytes_tx,bytes); 
+			/*printf("bytes_tx (read) %d\n",btx);*/
 		}
-		fflush(stdout);
 	}
 
 return NULL;
@@ -43,7 +44,7 @@ void* io_thread_read_process(void* unused){
     /*setvbuf(stdin,NULL,_IONBF,0);*/
     while(thread_run){
         int spaces = atomic_load(&bytes_rx);
-        if(spaces > 0)
+        if(spaces > 0){
             if((bytes = fread(buf,1,1,stdin)) != EOF){
                 if(bytes != 0){
                     write(read_fd[1],buf,bytes);
@@ -51,6 +52,13 @@ void* io_thread_read_process(void* unused){
                     /*fprintf(stderr,"Read byte: %c spaces: %d\n",*buf,spaces);*/
                 }
             }
+	    else {
+	      char nul = 0;
+	      write(read_fd[1],&nul, 1);
+	      atomic_fetch_sub(&bytes_rx, bytes);
+		
+	    }
+	}
 
     }
     return NULL;
@@ -60,7 +68,17 @@ uint8_t io_read_status(void){
     unsigned int btx = atomic_load(&bytes_tx);
     int brx = atomic_load(&bytes_rx);
     if(btx != 8 || brx == 8){
-        usleep(100);
+	int wr_prev = atomic_load(&write_previous);
+	if(!wr_prev){
+	    fd_set rfds;
+	    FD_ZERO(&rfds);
+	    FD_SET(read_fd[0],&rfds);
+	    struct timeval timeout;
+	    timeout.tv_sec = 0;
+	    timeout.tv_usec = 10000;
+	    select(1,&rfds, NULL, NULL, &timeout);
+	}
+	atomic_store(&write_previous,0);
         btx = atomic_load(&bytes_tx);
         brx = atomic_load(&bytes_rx);
     }
@@ -81,7 +99,9 @@ void io_init(){
     pthread_create(&io_thread_read, NULL, io_thread_read_process, NULL);
 }
 void cpu_thread_write_byte(uint8_t byte){
+    
     unsigned int bfree = atomic_load(&bytes_tx);
+    atomic_store(&write_previous, 1);
     if(bfree >0){
         while(!atomic_compare_exchange_strong(&bytes_tx,&bfree,bfree-1)){}
 	    write(fd[1], &byte, 1);
